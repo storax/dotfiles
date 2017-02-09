6b478a2d91a52ecb0c8da3c1d5814a1c2baa377d
David Zuber <zuber.david@gmx.de>
2017-02-09 20:56:16 +0000

    Install virtualenv pyenv plugins

--- zsh/.zsh.d/pyenv.zsh
+++ zsh/.zsh.d/pyenv.zsh
@@ -8,11 +8,12 @@
 
 # Download pyenv if it is not there yet
 [ -d ~/.pyenv/bin ] || git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+[ -d ~/pyenv/plugins/pyenv-virtualenv ] || git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 [ -d ~/.pyenv/plugins/pyenv-virtualenvwrapper ] || git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
 
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/projects/
-# source /usr/local/bin/virtualenvwrapper.sh
+source /usr/bin/virtualenvwrapper.sh
 
 # build python with enable shared
 export PYTHON_CONFIGURE_OPTS="--enable-unicode=ucs4 --enable-shared"
