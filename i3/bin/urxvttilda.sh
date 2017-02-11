c5c737f1ba65f6e062a123e9594e08ebe89a9651
David Zuber <zuber.david@gmx.de>
2017-02-11 22:48:13 +0000

    Fix urxvt startup script

--- i3/bin/urxvttilda.sh
+++ i3/bin/urxvttilda.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
-NEXT_WAIT_TIME=0
-COMMAND_STATUS=0
-until [[ $COMMAND_STATUS  -eq 0 || $NEXT_WAIT_TIME -eq 4 ]]; do
-    ping -c 1 -W 1 wttr.in
+NEXT_WAIT_TIME=1
+COMMAND_STATUS=-1
+until [[ $COMMAND_STATUS -eq 0 || $NEXT_WAIT_TIME -eq 6 ]]; do
+    curl wttr.in/london
     COMMAND_STATUS=$?
     sleep $NEXT_WAIT_TIME
     let NEXT_WAIT_TIME=NEXT_WAIT_TIME+1
@@ -10,7 +10,6 @@ done
 unset NEXT_WAIT_TIME
 unset COMMAND_STATUS
 
-curl wttr.in/london
 
 echo ""
 echo ""
