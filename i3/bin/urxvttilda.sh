5e659cd41f3d8a8fb1d889919f427a50c5dba512
David Zuber <zuber.david@gmx.de>
2017-02-11 20:04:22 +0000

    Wait until internet is there for wttr

--- i3/bin/urxvttilda.sh
+++ i3/bin/urxvttilda.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env zsh
+NEXT_WAIT_TIME=0
+COMMAND_STATUS=0
+until [[ $COMMAND_STATUS  -eq 0 || $NEXT_WAIT_TIME -eq 4 ]]; do
+    ping -c 1 -W 1 wttr.in
+    COMMAND_STATUS=$?
+    sleep $NEXT_WAIT_TIME
+    let NEXT_WAIT_TIME=NEXT_WAIT_TIME+1
+done
+unset NEXT_WAIT_TIME
+unset COMMAND_STATUS
+
 curl wttr.in/london
+
 echo ""
 echo ""
 echo ""
@@ -10,4 +22,4 @@ echo ""
 echo ""
 echo ""
 echo ""
-zsh
+exec zsh
